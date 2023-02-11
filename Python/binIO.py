532472e50ea904bbe201bd17db21350af538e8ab
weiyw16 <wei_yanwen@163.com>
2023-02-11 13:01:12 +0800

    add npy2bin, bin2npy

--- Python/binIO.py
+++ Python/binIO.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import numpy as np
 import os
 import struct
-
-
-# In[ ]:
 
 
 def readin_bin(path=None, seek_num=None, nt=None, nr=None):
     FA = open(path, "rb")
     FA.seek(seek_num)
     out_data = np.empty((nt,nr))
-#         for tt in range(self.nt):
     for rr in range(nr):
         for tt in range(nt):
             data = FA.read(4)
@@ -25,30 +18,24 @@ def readin_bin(path=None, seek_num=None, nt=None, nr=None):
     return out_data
 
 
-# In[ ]:
-
-
 def writeout_bin(path=None, in_data=None, seek_num=None, nt=None, nr=None):
     FA = open(path, "wb")
-#     FA.seek(seek_num)
-#     out_data = np.empty((nt,nr))
-#         for tt in range(self.nt):
     for rr in range(nr):
         for tt in range(nt):
             data = in_data[tt][rr] #FA.read(4)
             data_float = struct.pack("f", data)
-#             out_data[tt][rr] = data_float
             FA.write(data_float)
     FA.close()
 
 
-# In[1]:
+def npy2bin(inname=None, ouname=None, seek_num=0, nt=None, nr=None):
+  indata = np.load(inname)
+  writeout_bin(path=ouname, in_data=indata, seek_num=seek_num, nt=nt, nr=nr)
+  return indata
 
-
-
-
-# In[ ]:
-
-
+def bin2npy(inname=None, ouname=None, seek_num=0, nt=None, nr=None):
+  indata = readin_bin(path=inname, seek_num=seek_num, nt=nt, nr=nr)
+  np.save(ouname, indata)
+  return indata
 
 
